2022a244e93a29f7a51a5730a1ab37cf96da258b
Lio311 <lior31197@gmail.com>
2025-11-07 03:23:31 +0200

    Update check_stocks.py

--- check_stocks.py
+++ check_stocks.py
@@ -325,7 +325,7 @@ def get_gemini_insights(portfolio_details, general_market_losers, general_market
         "\n\n"
         "The response MUST be in ENGLISH."
     )
-    api_url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini‑1.5‑flash:generateContent?key={GEMINI_API_KEY}"
+    api_url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key={GEMINI_API_KEY}"
     
     payload = {
         "contents": [{
